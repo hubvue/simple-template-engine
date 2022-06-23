c5d0ce81a491055ec66ecfd133a4adc3aef531ab
hubvue <cckcc.kim@gmail.com>
2022-06-23 19:08:13 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+use regex::{Captures, Regex};
+use std::{collections::HashMap, fmt};
+
+enum Data {
+    Number(i32),
+    Boolean(bool),
+    Text(String),
+}
+
+impl fmt::Display for Data {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Text(x) => write!(f, "{}", x),
+            Self::Number(x) => write!(f, "{}", x),
+            Self::Boolean(x) => write!(f, "{}", x),
+        }
+    }
+}
+
+fn render(mut template: String, mut data: HashMap<&str, Data>) -> String {
+    let print_regex = Regex::new(r"\{\{(.*?)\}\}").unwrap();
+    template = print_regex
+        .replace_all(&template, |caps: &Captures| {
+            let key = caps.get(1).unwrap().as_str().trim();
+            data[key].to_string()
+        })
+        .to_string();
+
+    let repeat_regex =
+        Regex::new(r"\{% repeat (\d*?) times %\}((.|\n)*?)\{% endrepeat %\}").unwrap();
+    template = repeat_regex
+        .replace_all(&template, |caps: &Captures| {
+            let times = caps.get(1).unwrap().as_str().trim();
+            let code = caps.get(2).unwrap().as_str().trim();
+            code.repeat(times.parse::<usize>().unwrap())
+        })
+        .to_string();
+
+    let if_ealse_regex = Regex::new(
+        r"\{% if (.*?) %\}((.|\n)*?)(\{% else %\}((.|\n)*?)\{% endif %\}|\{% endif %\})",
+    )
+    .unwrap();
+    template = if_ealse_regex
+        .replace_all(&template, |caps: &Captures| {
+            let key = caps.get(1).unwrap().as_str().trim();
+            let if_code = caps.get(2).unwrap().as_str().trim();
+            let else_code = caps.get(5).map_or("", |m| m.as_str()).trim();
+
+            if let Data::Boolean(exp) = data[key] {
+                if exp {
+                    if_code.to_string()
+                } else {
+                    else_code.to_string()
+                }
+            } else {
+                "Error Parsing key".to_string()
+            }
+        })
+        .to_string();
+
+    template = template.replace("{#", "<!--").replace("#}", "-->");
+
+    template
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{render, Data};
+    use std::collections::HashMap;
+
+    #[test]
+    fn basic_tempalte() {
+        let input = std::fs::read_to_string("dist/index.html")
+            .expect("Something went wrong reading the file");
+        let data = HashMap::from([
+            ("hello", Data::Text("internet".to_string())),
+            ("allowed", Data::Boolean(true)),
+        ]);
+        println!("{}", render(input, data));
+    }
+}
